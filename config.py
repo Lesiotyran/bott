16e3fac9ce56c8f323975ca897c009863ccca203
Lesiotyran <sajkamichal4@gmail.com>
2025-04-14 03:28:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
 ROLE_ID = int(os.environ.get("DISCORD_ROLE_ID"))
+PORT = int(os.environ.get("PORT", 8080)) # Port, na którym ma nasłuchiwać serwer webowy
