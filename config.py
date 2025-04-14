3ddaa0030967f321768e6299a7b567a4e19a1c5e
Lesiotyran <sajkamichal4@gmail.com>
2025-04-14 03:14:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-TOKEN = 'MTI4MjM2MDA5MTA1MTI5NDg2Mg.G8DUCf.eJPhho9txikWbzJSkH6NHbfxRfzcb3RjCdtQMQ'
-ROLE_ID = 1276516323387506729 # ID roli uprawnionej do komend administracyjnych
+import os
+
+TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
+ROLE_ID = int(os.environ.get("DISCORD_ROLE_ID"))
